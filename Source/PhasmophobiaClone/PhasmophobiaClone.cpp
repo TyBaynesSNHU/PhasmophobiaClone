942e7d6f0f189e8052bea57180e4aba185d4d99c
Tyler Baynes <tyler.baynes@snhu.edu>
2025-10-01 11:05:35 -0700

    Intial project set up and commit.

---
+++ Source/PhasmophobiaClone/PhasmophobiaClone.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PhasmophobiaClone.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PhasmophobiaClone, "PhasmophobiaClone" );
+
+DEFINE_LOG_CATEGORY(LogPhasmophobiaClone)
---
+++ Source/PhasmophobiaClone/PhasmophobiaCloneGameMode.h
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PhasmophobiaCloneGameMode.generated.h"
+
+/**
+ *  Simple GameMode for a first person game
+ */
+UCLASS(abstract)
+class APhasmophobiaCloneGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APhasmophobiaCloneGameMode();
+};
+
+
+
